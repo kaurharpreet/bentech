9b26cbfd8a60976bfd70a341146e941ec2cda2e9
Harpreet Kaur <kaur.harpreet007@gmail.com>
2024-01-31 11:35:12 +0530

    project_setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const indexRouter = require("./routes/index");
+const db = require("./config/database");
+
+const app = express();
+app.use(express.json());
+app.use(process.env.API_BASE_PATH, indexRouter);
+app.listen(port, () => {
+    console.log(`Server is running at: http://localhost:${process.env.PORT}`);
+});
+
+const initApp = () => {
+    // Test the connection.
+    try {
+        db.authenticate();
+        console.log("Connection has been established successfully.");    
+    } catch (error) {
+       console.error("Unable to connect to the database:", error.original);
+    }
+ };
+ 
+ /* Initialize the application. */
+ initApp();
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let router = express.Router();
+const user = require("../controllers/users");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+});
+
+
+
+module.exports = router;
